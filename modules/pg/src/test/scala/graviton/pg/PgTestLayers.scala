384b575db2c2bf4a2fa341dbd2956367635227ec
Adriel Casellas <adrielcasellas@gmail.com>
2025-09-03 20:26:50 -0600

    test(pg): run Postgres via Testcontainers

--- modules/pg/src/test/scala/graviton/pg/PgTestLayers.scala
+++ modules/pg/src/test/scala/graviton/pg/PgTestLayers.scala
@@ -1,34 +1,46 @@
 package graviton.pg
 
 import zio.*
-import io.zonky.test.db.postgres.embedded.EmbeddedPostgres
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.utility.DockerImageName
 import org.postgresql.ds.PGSimpleDataSource
-import javax.sql.DataSource
 import com.augustnagro.magnum.*
+import javax.sql.DataSource
 import scala.io.Source
 
 object PgTestLayers:
-  private def startEmbedded: ZIO[Scope, Throwable, EmbeddedPostgres] =
-    ZIO.acquireRelease(ZIO.attempt(EmbeddedPostgres.start()))(pg => ZIO.attempt(pg.close()).ignore)
+  private def startContainer: ZIO[Scope, Throwable, PostgreSQLContainer[?]] =
+    ZIO.acquireRelease {
+      ZIO.attempt {
+        val c =
+          new PostgreSQLContainer(DockerImageName.parse("postgres:16-alpine"))
+        c.start()
+        c
+      }
+    }(c => ZIO.attempt(c.stop()).ignore)
 
   private def runDdl(ds: DataSource): Task[Unit] =
     ZIO.attemptBlocking {
-      val sql  = Source.fromResource("ddl.sql").mkString
+      val sql = Source.fromResource("ddl.sql").mkString
       val conn = ds.getConnection()
-      val stmt = conn.createStatement()
-      stmt.execute(sql)
-      // intentionally not closing to keep connection alive for tests
+      try
+        val stmt = conn.createStatement()
+        stmt.execute(sql)
+        stmt.close()
+      finally conn.close()
     }
 
   val transactorLayer: ZLayer[Any, Throwable, Transactor] =
     ZLayer.scoped {
       for
-        pg <- startEmbedded
-        ds  <- ZIO.attempt {
-                val d = PGSimpleDataSource()
-                d.setURL(pg.getJdbcUrl("postgres", "postgres"))
-                d
-              }
-        _  <- runDdl(ds)
+        c <- startContainer
+        ds <- ZIO.attempt {
+          val d = PGSimpleDataSource()
+          d.setURL(c.getJdbcUrl)
+          d.setUser(c.getUsername)
+          d.setPassword(c.getPassword)
+          d
+        }
+        _ <- runDdl(ds)
       yield Transactor(ds)
     }
