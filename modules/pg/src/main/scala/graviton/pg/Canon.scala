f617c53cafa64b37d21ee2c02c226bda38d22d31
Adriel Casellas <adrielcasellas@gmail.com>
2025-09-30 03:56:46 -0600

    chore: run scalafmt and document requirement

--- modules/pg/src/main/scala/graviton/pg/Canon.scala
+++ modules/pg/src/main/scala/graviton/pg/Canon.scala
@@ -1,30 +1,3 @@
 package graviton.pg
 
-import zio.Chunk
-import zio.schema.DynamicValue
-import zio.schema.codec.JsonCodec.ExplicitConfig
-import zio.schema.codec.JsonCodec
-
-object Canon:
-  private val codec = JsonCodec.schemaBasedBinaryCodec[DynamicValue](
-    JsonCodec.Configuration(
-      explicitNulls = ExplicitConfig(encoding = false),
-      explicitEmptyCollections = ExplicitConfig(encoding = false),
-    )
-  )
-
-  def canonicalize(dv: DynamicValue): Chunk[Byte] =
-    codec.encode(dv)
-
-  def storeKey(
-    implId: String,
-    dvCanon: Chunk[Byte],
-    buildFp: Chunk[Byte],
-    H: Chunk[Byte] => Chunk[Byte],
-  ): StoreKey =
-    val sep      = Chunk.single(0.toByte)
-    val material = Chunk.fromArray(
-      implId.getBytes("UTF-8")
-    ) ++ sep ++ dvCanon ++ sep ++ buildFp
-
-    StoreKey.applyUnsafe(H(material))
+export graviton.db.Canon.*
