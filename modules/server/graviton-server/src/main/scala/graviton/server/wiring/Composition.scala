cdfe7ac3efc71bed1e9ea85b594c86eca2b2fc78
Adriel Casellas <adrielcasellas@gmail.com>
2025-10-03 04:54:45 -0600

    Restore scan abstraction and fix docs workflow

--- modules/server/graviton-server/src/main/scala/graviton/server/wiring/Composition.scala
+++ modules/server/graviton-server/src/main/scala/graviton/server/wiring/Composition.scala
@@ -7,10 +7,10 @@ import zio.{Layer, ZIO, ZLayer}
 
 object Composition:
   private final class NoopBlobStore extends BlobStore:
-    override def put: ZSink[Any, Throwable, Byte, Nothing, BlobWriteResult] =
+    override def put: ZSink[Any, Throwable, Byte, Nothing, BlobWriteResult]                     =
       ZSink.fail(new UnsupportedOperationException("BlobStore wiring not configured"))
-    override def get(key: graviton.core.keys.BinaryKey): ZStream[Any, Throwable, Byte]           = ZStream.empty
-    override def stat(key: graviton.core.keys.BinaryKey): ZIO[Any, Throwable, Option[BlobStat]]  = ZIO.succeed(None)
-    override def delete(key: graviton.core.keys.BinaryKey): ZIO[Any, Throwable, Unit]            = ZIO.unit
+    override def get(key: graviton.core.keys.BinaryKey): ZStream[Any, Throwable, Byte]          = ZStream.empty
+    override def stat(key: graviton.core.keys.BinaryKey): ZIO[Any, Throwable, Option[BlobStat]] = ZIO.succeed(None)
+    override def delete(key: graviton.core.keys.BinaryKey): ZIO[Any, Throwable, Unit]           = ZIO.unit
 
   val live: Layer[Nothing, BlobStore] = ZLayer.succeed(new NoopBlobStore)
