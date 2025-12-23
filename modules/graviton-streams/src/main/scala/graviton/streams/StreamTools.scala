4875f582d3f70d87f5e3f788dd964512f75046fb
Adriel Casellas <adrielcasellas@gmail.com>
2025-12-22 20:46:01 -0700

    Implement ChunkingSink and refactor StreamTools for enhanced streaming capabilities
    - Introduced ChunkingSink for efficient chunk processing in ZIO streams.
    - Refactored StreamTools to utilize ChunkingSink in the teeTo method, improving data handling.
    - Updated BuildHelper to organize settings and improve test execution configurations.
    - Removed obsolete AccessorBuilder, Schema, TypeId, and ExtensibleMetaSchema files to streamline the codebase.

--- modules/graviton-streams/src/main/scala/graviton/streams/StreamTools.scala
+++ modules/graviton-streams/src/main/scala/graviton/streams/StreamTools.scala
@@ -1,7 +1,40 @@
 package graviton.streams
 
-import zio.stream.{ZSink, ZStream}
+import zio.Chunk
+import zio.stream.{ZSink, ZPipeline}
+
+type ChunkingSink[A] = ZSink[Any, Nothing, A, A, Chunk[A]]
+
+object ChunkingSink:  
+  
+  // default to 516k chunks
+
+  def fixed(limit: Int = 516 * 1024): ForAny[ChunkingSink] = 
+    ForAny.ChunkingSink([?+] => () => ZSink.collectAllN(limit))
+
+end ChunkingSink
+
+trait ForAny[F[_]]:
+
+	def apply[A]: F[A]
+
+end ForAny
+
+object ForAny:
+
+  def apply[F[_]]: ([?+] => (() => F[?+])) => ForAny[F] = f => new: 
+    def apply[A]: F[A] = f()
+
+  val ChunkingSink = ForAny[ChunkingSink]
+
+end ForAny
 
 object StreamTools:
-  def teeTo[R, E, A](stream: ZStream[R, E, A], sink: ZSink[R, E, A, Any, Any]): ZStream[R, E, A] =
-    stream.tap(value => ZStream.succeed(value).run(sink).unit)
+
+  def teeTo[R, E, A](
+    sink: ZSink[R, E, A, A, Any],
+    rechunker: ChunkingSink[A] = ChunkingSink.fixed()[A]
+  ): ZPipeline[R, E, A, A] =
+    ZPipeline.fromFunction(_.transduce(sink zipParRight rechunker).flattenChunks)
+
+end StreamTools
